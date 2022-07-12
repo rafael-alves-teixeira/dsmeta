8c5a635ae41593ded430f77a47dbfb2fe0af25b1
rafael-alves-teixeira <rafael.info.tc@gmail.com>
2022-07-12 16:20:16 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsmeta/ProjetoFullStackSpringReactDevsuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFullStackSpringReactDevsuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFullStackSpringReactDevsuperiorApplication.class, args);
+	}
+
+}
